06a6e2b805b4a42c21e840a6a7474eac925f0bef
tomoya2025 <t.momo1023@gmail.com>
2025-03-04 00:05:57 +0900

    Add files via upload

---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,8 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18{
+	
+	public void eachIntroduce() {
+		System.out.println("私はJavaが得意です");
+	}
+}
---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,26 @@
+package kadai_018;
+
+abstract public class Kato_Chapter18 {
+	
+	String familyName = "加藤";
+    String givenName;
+	String address = "東京都中野区〇×";
+	
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+	
+	public void commonIntroduce() {
+		System.out.println("名前は" + familyName + givenName + "です");
+		System.out.println("住所は" + address + "です");
+	}
+	abstract void eachIntroduce();
+	
+	
+	
+	public void execIntroduce() {
+		  commonIntroduce();
+		  eachIntroduce();
+	}
+	
+}
